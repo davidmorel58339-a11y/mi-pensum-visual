23110a844fc7fd9e3c1e7a6f6e8d1ae883fab507
David Morel <davidmorel58339@gmail.com>
2026-05-30 22:11:36 -0400

    eliminar speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import 'reactflow/dist/style.css';
 import SubjectNode from './SubjectNode';
 import TrimesterNode from './TrimesterNode';
 import CustomEdge from './CustomEdge';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const nodeTypes = { subject: SubjectNode, trimester: TrimesterNode };
 const edgeTypes = { customArch: CustomEdge };
